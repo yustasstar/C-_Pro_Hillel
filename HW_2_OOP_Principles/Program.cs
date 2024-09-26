369c4397d4ea0a57a27af5410f8ce9133963ed8b
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-09-27 00:01:03 +0300

    Task3_OOP

--- HW_2_OOP_Principles/Program.cs
+++ HW_2_OOP_Principles/Program.cs
@@ -2,7 +2,8 @@
 using HW_2_OOP_Principles.Product.Product;
 
 //Task1_OOP();
-Task2_OOP();
+//Task2_OOP();
+Task3_OOP();
 
 static void Task1_OOP()
 {
@@ -31,6 +32,24 @@ static void Task2_OOP()
         instrument.Description();
         instrument.History();
         instrument.Sound();
-        Console.WriteLine("--------------------------------------------"); 
+        Console.WriteLine("--------------------------------------------");
     }
+}
+
+static void Task3_OOP()
+{
+    int numberToConvert = 255;
+    DecimalNumber number = new(numberToConvert);
+
+    Console.WriteLine($"Decimal number = {numberToConvert} : Binary number = {number.ToBinary()}");
+    Console.WriteLine($"Decimal number = {numberToConvert} : Octal number = {number.ToOctal()}");
+    Console.WriteLine($"Decimal number = {numberToConvert} : Hexadecimal number = {number.ToHexadecimal()}");
+}
+
+internal readonly struct DecimalNumber(int value)
+{
+    private readonly int _value = value;
+    public readonly string ToBinary() => Convert.ToString(_value, 2);
+    public readonly string ToOctal() => Convert.ToString(_value, 8);
+    public readonly string ToHexadecimal() => Convert.ToString(_value, 16).ToUpper();
 }
