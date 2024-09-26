424a45d9c52ca4bc5cbecff3657e9120bca8713b
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-09-26 14:51:13 +0300

    Task_1 (OOP)

---
+++ HW_2_OOP_Principles/Program.cs
@@ -0,0 +1,9 @@
+﻿using HW_2_OOP_Principles;
+
+Product product = new Product();
+
+product.SetProductName();
+product.SetProductPrice();
+product.DisplayProduct();
+product.ReducePrice();
+product.DisplayProduct();
