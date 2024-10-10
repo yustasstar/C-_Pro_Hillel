a7e9bfca1cb53b5958686287c9b47abc6f4ed5de
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-10-10 21:46:27 +0300

    lesson 7 added

---
+++ Lesson_7/BadSolution.cs
@@ -0,0 +1,50 @@
+﻿//namespace IDisposableForCleanup
+//{
+//    using System;
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            DisplayWelcomeNotes();
+//            DoSomeWork();
+//            DisplayExitNotes();
+//        }
+
+//        private static void DoSomeWork()
+//        {
+//            Console.WriteLine("doing some work");
+
+//            try
+//            {
+//                Console.WriteLine("and doing more work");
+//                throw new Exception("Some dummy exception that we can survive");
+//            }
+//            catch
+//            {
+//                Console.ForegroundColor = ConsoleColor.Red;
+//                Console.WriteLine("oops there was an exception but I was able to survive it");
+//                Console.ForegroundColor = ConsoleColor.White;
+//            }
+
+//            Console.WriteLine("My work is done...");
+//        }
+
+//        private static void DisplayWelcomeNotes()
+//        {
+//            Console.ForegroundColor = ConsoleColor.Green;
+//            Console.WriteLine("Welcome to this very useful app");
+//            Console.ForegroundColor = ConsoleColor.White;
+//        }
+
+//        private static void DisplayExitNotes()
+//        {
+//            Console.ForegroundColor = ConsoleColor.Green;
+//            Console.WriteLine();
+//            Console.WriteLine("Thanks for using this very useful app");
+//            Console.WriteLine("Press enter to exit");
+//            Console.ForegroundColor = ConsoleColor.White;
+//            Console.ReadLine();
+//        }
+//    }
+//}
---
+++ Lesson_7/GoodSolution.cs
@@ -0,0 +1,69 @@
+﻿//namespace IDisposableAfterCleanup
+//{
+//    using System;
+
+//    public class ForeColor : IDisposable
+//    {
+//        private readonly ConsoleColor _previousColor;
+
+//        public ForeColor(ConsoleColor foreColor)
+//        {
+//            _previousColor = Console.ForegroundColor;
+//            Console.ForegroundColor = foreColor;
+//        }
+
+//        public void Dispose()
+//        {
+//            Console.ForegroundColor = _previousColor;
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            DisplayWelcomeNotes();
+//            DoSomeWork();
+//            DisplayExitNotes();
+//        }
+
+//        private static void DoSomeWork()
+//        {
+//            Console.WriteLine("doing some work");
+
+//            try
+//            {
+//                Console.WriteLine("and doing more work");
+//                throw new Exception("Some dummy exception that we can survive");
+//            }
+//            catch
+//            {
+//                using (new ForeColor(ConsoleColor.Red))
+//                {
+//                    Console.WriteLine("oops there was an exception but I was able to survive it");
+//                }
+//            }
+
+//            Console.WriteLine("My work is done...");
+//        }
+
+//        private static void DisplayWelcomeNotes()
+//        {
+//            using (new ForeColor(ConsoleColor.Green))
+//            {
+//                Console.WriteLine("Welcome to this very useful app");
+//            }
+//        }
+
+//        private static void DisplayExitNotes()
+//        {
+//            using (new ForeColor(ConsoleColor.Green))
+//            {
+//                Console.WriteLine();
+//                Console.WriteLine("Thanks for using this very useful app");
+//                Console.WriteLine("Press enter to exit");
+//                Console.ReadLine();
+//            }
+//        }
+//    }
+//}
