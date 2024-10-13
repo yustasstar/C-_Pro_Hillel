3376d520a7853e29a1328359021d244290cd28d3
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-10-13 20:17:21 +0300

    HW_5_Finished

--- HW_5_GarbageCollector/Program.cs
+++ HW_5_GarbageCollector/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using HW_5_GarbageCollector;
+
+using (Play play = new Play("Hamlet", "William Shakespeare", "Tragedy", 1603))
+{
+    play.Read();
+    play.Update(authorFullName: "Ivanov Vasyliy", year: 999);
+    play.Read();
+}
+Console.WriteLine("-------------------------------------");
+
+Play play2 = new Play("Othello", "William Shakespeare", "Tragedy", 1604);
+play2.Read();
+play2.Update(authorFullName:"Name Updated", title:"New Title");
+play2.Read();
+
+play2.Dispose();
+play2.Read();
+Console.WriteLine("**************************************");
+
+using (Shop shop = new Shop("Good Market", "123 Main St", "Grocery"))
+{
+    shop.Read();
+    shop.Update(shopName: "Shop Name Updated", address: "New Address");
+    shop.Read();
+}
+Console.WriteLine("-------------------------------------");
+
+Shop shop2 = new Shop("Hardware Hub", "456 Industrial Rd", "Hardware");
+shop2.Read();
+shop2.Update(shopName: "Shop Name Updated", address: "New Address");
+shop2.Read();
+
+shop2.Dispose();
+shop2.Read();
---
+++ HW_5_GarbageCollector/Shop.cs
@@ -0,0 +1,75 @@
+﻿namespace HW_5_GarbageCollector
+{
+    public class Shop : IDisposable
+    {
+        public string? ShopName { get; set; }
+        public string? Address { get; set; }
+        public string? ShopType { get; set; }
+
+        private bool disposed = false;
+
+        public Shop(string? shopName, string? address, string? shopType)
+        {
+            ShopName = shopName;
+            Address = address;
+            ShopType = shopType;
+        }
+
+        ~Shop()
+        {
+            Dispose(false);
+            Console.WriteLine("Destructor invoked");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+            Console.WriteLine("GC.SuppressFinalize invoked");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("managed resources cleared");
+                }
+                Delete();
+                Console.WriteLine("unmanaged resources cleared");
+                disposed = true;
+            }
+        }
+
+        public void Create(string? shopName, string? address, string? shopType)
+        {
+            ShopName = shopName;
+            Address = address;
+            ShopType = shopType;
+            Console.WriteLine("Data created!");
+        }
+
+        public void Read()
+        {
+            Console.WriteLine($"Shop Name: {ShopName ?? "N/A"}, Address: {Address ?? "N/A"}, Type: {ShopType ?? "N/A"}");
+        }
+
+        public void Update(string? shopName = null, string? address = null, string? shopType = null)
+        {
+            ShopName = shopName ?? ShopName;
+            Address = address ?? Address;
+            ShopType = shopType ?? ShopType;
+            Console.WriteLine("Data updated!");
+        }
+
+        public void Delete()
+        {
+            ShopName = null;
+            Address = null;
+            ShopType = null;
+            Console.WriteLine("Data cleared!");
+        }
+    }
+
+}
