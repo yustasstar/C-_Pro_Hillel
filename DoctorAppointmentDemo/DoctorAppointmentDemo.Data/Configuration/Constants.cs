9966596647c525d8205fb0c5541e84b62e0c360e
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-10-28 17:25:36 +0200

    save

--- DoctorAppointmentDemo/DoctorAppointmentDemo.Data/Configuration/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyDoctorAppointment.Data.Configuration
-{
-    public static class Constants
-    {
-        // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
-        public const string AppSettingsPath = "C:\\Users\\admin\\source\\repos\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
-    }
-}
