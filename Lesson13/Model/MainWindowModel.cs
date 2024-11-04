b4692e513ea389dd2d84322f52113c9449a30f5c
Iurii Sylaiev <iuriisylaiev@gmail.com>
2024-11-04 14:47:30 +0200

    lesson 13 added

---
+++ Lesson13/Model/MainWindowModel.cs
@@ -0,0 +1,27 @@
+﻿using Model;
+using System.Threading;
+using System.Windows.Controls;
+using static Model.FileCopier;
+
+namespace CopyFilesWPF.Model
+{
+    public class MainWindowModel
+    {
+        public FilePath FilePath { get; set; }
+
+        public MainWindowModel() {
+            FilePath = new FilePath();
+        }
+
+        public void CopyFile(ProgressChangeDelegate onProgressChanged, CompleteDelegate onComplete, Grid gridPanel)
+        {
+            var copier = new FileCopier(FilePath, onProgressChanged, onComplete, gridPanel);
+            gridPanel.Tag = copier;
+            var newCopierThread = new Thread(new ThreadStart(copier.CopyFile))
+            {
+                IsBackground = true
+            };
+            newCopierThread.Start();
+        }
+    }
+}
